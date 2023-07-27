338a67dc0251a28355a0ed1ca8573c7799a5086c
Andrii Maliarenko <106884639+adrilix@users.noreply.github.com>
2023-07-27 19:10:31 +0200

    teper tochno ))

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import PropTypes from 'prop-types';
 import Actor from 'components/Actor/Actor';
 import { LoaderSpinner } from 'components/Loader/Loader';
 import Message from 'components/Message/Message'
